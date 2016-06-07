609267d42aa6717d1e711857712d602e9f5f240f
Jonathan Espinoza <cetecom@LC08L1313>
2016-06-06 22:02:33 -0300

    J.E.B. Carga de atributos, constructores y mutadores de clases.

--- clases/Cliente.php
+++ clases/Cliente.php
@@ -1,15 +1,8 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Cliente
  *
- * @author cetecom
+ * @author Jonathan Espinoza
  */
 class Cliente {
     
@@ -19,6 +12,83 @@ class Cliente {
     private $csexo;
     private $dfechanacimiento;
     private $semail;
+    private $snomusuario;
+    private $spass;
     
-    
+    function __construct($srun, $snombre, $apellidos, $csexo, $dfechanacimiento, $semail, $snomusuario, $spass) {
+        $this->srun = $srun;
+        $this->snombre = $snombre;
+        $this->apellidos = $apellidos;
+        $this->csexo = $csexo;
+        $this->dfechanacimiento = $dfechanacimiento;
+        $this->semail = $semail;
+        $this->snomusuario = $snomusuario;
+        $this->spass = $spass;
+    }
+
+    function getSrun() {
+        return $this->srun;
+    }
+
+    function getSnombre() {
+        return $this->snombre;
+    }
+
+    function getApellidos() {
+        return $this->apellidos;
+    }
+
+    function getCsexo() {
+        return $this->csexo;
+    }
+
+    function getDfechanacimiento() {
+        return $this->dfechanacimiento;
+    }
+
+    function getSemail() {
+        return $this->semail;
+    }
+
+    function getSnomusuario() {
+        return $this->snomusuario;
+    }
+
+    function getSpass() {
+        return $this->spass;
+    }
+
+    function setSrun($srun) {
+        $this->srun = $srun;
+    }
+
+    function setSnombre($snombre) {
+        $this->snombre = $snombre;
+    }
+
+    function setApellidos($apellidos) {
+        $this->apellidos = $apellidos;
+    }
+
+    function setCsexo($csexo) {
+        $this->csexo = $csexo;
+    }
+
+    function setDfechanacimiento($dfechanacimiento) {
+        $this->dfechanacimiento = $dfechanacimiento;
+    }
+
+    function setSemail($semail) {
+        $this->semail = $semail;
+    }
+
+    function setSnomusuario($snomusuario) {
+        $this->snomusuario = $snomusuario;
+    }
+
+    function setSpass($spass) {
+        $this->spass = $spass;
+    }
+
+
 }
--- clases/Producto.php
+++ clases/Producto.php
@@ -1,16 +1,94 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Producto
  *
- * @author cetecom
+ * @author Jonathan Espinoza
  */
 class Producto {
-    //put your code here
+    
+    private $scodigo;
+    private $nprecio;
+    private $sdescripcion;
+    private $subicacion;
+    private $ncantidad;
+    private $scategoria;
+    private $ndescuento;
+    private $dterminodescuento;
+    
+    function __construct($scodigo, $nprecio, $sdescripcion, $subicacion, $ncantidad, $scategoria, $ndescuento, $dterminodescuento) {
+        $this->scodigo = $scodigo;
+        $this->nprecio = $nprecio;
+        $this->sdescripcion = $sdescripcion;
+        $this->subicacion = $subicacion;
+        $this->ncantidad = $ncantidad;
+        $this->scategoria = $scategoria;
+        $this->ndescuento = $ndescuento;
+        $this->dterminodescuento = $dterminodescuento;
+    }
+
+    function getScodigo() {
+        return $this->scodigo;
+    }
+
+    function getNprecio() {
+        return $this->nprecio;
+    }
+
+    function getSdescripcion() {
+        return $this->sdescripcion;
+    }
+
+    function getSubicacion() {
+        return $this->subicacion;
+    }
+
+    function getNcantidad() {
+        return $this->ncantidad;
+    }
+
+    function getScategoria() {
+        return $this->scategoria;
+    }
+
+    function getNdescuento() {
+        return $this->ndescuento;
+    }
+
+    function getDterminodescuento() {
+        return $this->dterminodescuento;
+    }
+
+    function setScodigo($scodigo) {
+        $this->scodigo = $scodigo;
+    }
+
+    function setNprecio($nprecio) {
+        $this->nprecio = $nprecio;
+    }
+
+    function setSdescripcion($sdescripcion) {
+        $this->sdescripcion = $sdescripcion;
+    }
+
+    function setSubicacion($subicacion) {
+        $this->subicacion = $subicacion;
+    }
+
+    function setNcantidad($ncantidad) {
+        $this->ncantidad = $ncantidad;
+    }
+
+    function setScategoria($scategoria) {
+        $this->scategoria = $scategoria;
+    }
+
+    function setNdescuento($ndescuento) {
+        $this->ndescuento = $ndescuento;
+    }
+
+    function setDterminodescuento($dterminodescuento) {
+        $this->dterminodescuento = $dterminodescuento;
+    }
+
+
 }
