986de86b750e5251bcf69b4fd80584f90c629dae
Jonathan Espinoza <cetecom@LC08L1319>
2016-06-08 21:53:46 -0300

    J.E.B. Agregado de clases para funcionalidad de la pagina

---
+++ constantes.php
@@ -0,0 +1,5 @@
+<?php
+define("PATHAPP",$_SERVER['DOCUMENT_ROOT']."/CSA/");
+define("PATHURL","http://localhost:81/CSA/");
+define("TITULOAPP","Super WebSite");
+?>
---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+include 'librerias.php';
+session_start();
+session_unset();
+?>
+<script>
+	document.location.href="index.php";
+</script>
