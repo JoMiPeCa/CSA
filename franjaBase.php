cad9c10d59911bf47bfb21506cb69881cb173967
Jorge Muñoz N. <jmunoznov@gmail.com>
2016-06-13 22:09:16 -0300

    se reestructura vista de la pagina web, usando menus....aún falta

---
+++ franjaBase.php
@@ -0,0 +1,47 @@
+<!--BASE-->
+<div align="center">
+    <table width="100%"  border="0" cellspacing="0" cellpadding="0">
+        <tr>
+            <td width="36%" align="center"><table width="306" height="119"  border="0" cellpadding="0" cellspacing="0" background="images/fondo_datosdecontacto.gif">
+                    <tr>
+                        <td valign="middle"><table width="95%"  border="0" align="center" cellpadding="2" cellspacing="0">
+                                <tr>
+                                    <td><img src="images/pixel_trans.gif" width="1" height="5"></td>
+                                </tr>
+                                <tr>
+                                    <td>Tel&eacute;fono tienda online: +34 680 60 74 86 </td>
+                                </tr>
+                                <tr>
+                                    <td>Por email: <a href="mailto:ventas@ferreteriaortiz.es" class="linkamarillo">ventas@ferreteriaortiz.es</a> </td>
+                                </tr>
+                                <tr>
+                                    <td>A trav&eacute;s de los tel&eacute;fonos de <a href="address.aspx" target="_blank" class="linkamarillo">Nuestras tiendas</a> </td>
+                                </tr>
+                            </table></td>
+                    </tr>
+                </table></td>
+            <td width="37%" align="center"><table width="306" height="119"  border="0" cellpadding="0" cellspacing="0" background="images/fondo_ayuda.gif">
+                    <tr>
+                        <td valign="middle"><table width="95%"  border="0" align="center" cellpadding="2" cellspacing="0">
+                                <tr>
+                                    <td><img src="images/pixel_trans.gif" width="1" height="5"></td>
+                                </tr>
+                                <tr>
+                                    <td>Gestionar mis datos: <a href="checkout1.aspx" class="linkamarillo">&Aacute;rea de cliente</a> </td>
+                                </tr>
+                                <tr>
+                                    <td>He olvidado mi contrase&ntilde;a: <a href="checkout1.aspx" class="linkamarillo">Recuperar</a> </td>
+                                </tr>
+                                <tr>
+                                    <td>Consultar: <a href="generalConditions.aspx" target="_blank" class="linkamarillo">Condiciones generales, cambios y devoluciones </a> </td>
+                                </tr>
+                            </table></td>
+                    </tr>
+                </table></td>
+
+        </tr>
+    </table>
+    <br>
+    <br>
+</div>
+<!--BASE-->
