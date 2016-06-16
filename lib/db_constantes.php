d4f827d17dba703c4fecd4e55d8acd8fd0b4a7a5
Joseph <jose.perez@peluka.info>
2016-06-15 21:36:09 -0300

    db_funciones fixed db_constantes added

---
+++ lib/db_constantes.php
@@ -0,0 +1,7 @@
+<?php
+/*Definición opciones de conexión*/
+define("MYSQL_SERVER","mysql:host=localhost");
+define("MYSQL_USER","root");
+define("MYSQL_PASS","avaras08");
+define("MYSQL_DB","dbname=ferreteriaweb");
+?>
--- lib/db_funciones.php
+++ lib/db_funciones.php
@@ -1,5 +1,9 @@
 <?php
-
-
-
-/*sdgfsag*/
+include 'db_constantes.php';
+function dbconnect(){
+	$db=new PDO(MYSQL_SERVER.";".MYSQL_DB,MYSQL_USER,MYSQL_PASS);
+	$db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $db;
+}
+?>
