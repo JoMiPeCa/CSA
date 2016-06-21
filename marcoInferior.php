85a59d1b9922f995b3455f0f5c71666e418cfee8
Joseph Perez <jose.perez@peluka.info>
2016-06-20 23:09:31 -0300

    Fixed Index. Added Frame for top/bottom

---
+++ marcoInferior.php
@@ -0,0 +1,24 @@
+<!-- FIN BANDEJON CENTRAL -->
+                            </td>
+                            <td bgcolor="#ffffff" width="168" align="left" valign="top">
+                                <?php require './franjaDerecha.php'; ?>
+                                <br>
+                                <br>
+                                <br>
+                            </td>
+                        </tr>
+
+                    </table>          
+                    <map name="Map">
+                        <area shape="rect" coords="127,8,171,25" href="#" alt="Ofertas">
+                        <area shape="rect" coords="183,9,256,25" href="#" alt="Novedades">
+                        <area shape="rect" coords="265,8,360,25" href="#" alt="Los m&aacute;s vendidos">
+                        <area shape="rect" coords="367,9,434,25" href="#" alt="Listado AZ">
+                    </map>
+                </div>
+                <?php require 'franjaBase.php'; ?>
+            </td>
+        </tr>
+    </table>
+</body>
+
