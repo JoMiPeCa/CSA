baafef34d8e156412c4af826216ee317f49b5a0f
Joseph Perez <jose.perez@peluka.info>
2016-06-20 21:54:18 -0300

    Fixed Index Tags

--- index.php
+++ index.php
@@ -2,22 +2,21 @@
     <table width="780" border="0" align="center" valign="top" cellpadding="0" cellspacing="0">
         <tr>
             <td valign="top" bgcolor="#ffffff">
-        <html>
-            <head>
-                <TITLE>Ferreteria Ortiz - Ferretería Ortiz</TITLE>
-                <META id="_TopLeft_metaTitle" NAME="Title" content="Ferreteria Ortiz. Tienda online con más de 20.000 productos - Ferretería Ortiz"></META>
-                <META id="_TopLeft_metaSubject" NAME="Subject" content="Ferretería Ortiz. Tienda online con más de 20.000 productos."></META>
-                <META id="_TopLeft_metaDescription" NAME="Description" content="Ferretería Ortiz. Tienda online con más de 20.000 productos. Herrajes para obra y decoración, metalistería, cerrajería, accesorios para baño, herramientas, ferretería en general."></META>
-                <META id="_TopLeft_metaKeywords" NAME="Keywords" content="Abrazaderas, Accesorios ba&amp;ntilde;o, Aceites, Adhesivos, Adornos, Alambres, Alcayatas, Alcotanas, Aldaba, Aldabilla, Alicates, Alzapa&amp;ntilde;os, Amaestramiento, Amoladoras, Angulo, Anillas, Antipanicos, Antirrobos, Aplacadoras, Apliques, Apliques para luz, Aprieto, Arandelas, Archivadores, Arcos de segueta, Arcos de sierra, Armeros, Asas, Aspiradores, Avellanadores, Barandilla, Barras, Barrenas, Baterias, Berbiquis, Bisagras, Bloqueador, Bocallaves, Bombillos, botiquines, Brocas, Burletes, Buzones, Cadenas, Cajas fuertes, Calibres, Campanas, Candados, Cantoneras, capitel, Cariatides, Carretilla, Carriles, Cartelas, casquillos, Cazoletas, Cepillos, Cerraderos, Cerraduras, Cerraduras mueble, Cerrojillos, Cerrojo, Cestas, Chapas, Cierrapuertas, Cierre, Cinceles, Cinturoneros, Cizallas, Clavadoras, Clavos, Colgantes, Collarines, Compases, Compresores, Condena, Corbateros, Coronas, Correderas, Cortador, Cortafrios, Cortatubos, Cremonas, Cuadradillo, Cuchillos, Cuerda, Cutters, Decaparadores, Desbloqueo, Destornilladores, Dosificadores, Dremel, Electricidad, Electrodos, Engletadoras, Ensambles, Escaleras, Escobilleros, Escofinas, Escuadras, Eses, Espatulas, Espejos, Fallebas, Fieltros, Figuras, Filete, Flesadoras, Flexometros, Formones, Ganchos, Garras, Garrotas, Gramiles, Granetes, Grapadoras, Grapas, Griferias, Grisans, Guantes, Gubias, Guias, Guillamenes, Hachas, Hembrillas, Herramientas, Husillos, Imanes, Ingletadoras, Jaboneras, Lavabos, Letras, Letreros, Lija, Lijadoras, Limas, Limpieza, Linternas, Llamadores, Llanas, Llaveros, Llaves, Lubricantes, Macetas, Macollas, Mamparas, Mangos, Manilla, Manillon, Martillos, Mascaras, Mazas, Mecanismos, Metopas, Mirillas, Moldura, Muebles, Muelas, Muelles, Muletillas, Multiusos, niveles, Nudos, Numeros, Paletas, Pantaloneros, Papeleras, Pasacables, Pasadores, Pasamanos, Pasamonedas, Patas, Pegamentos, Perchas, Pernios, Picaporte, Piedras, Pinchos, Pinzas, pistola, Placa limpieza, Plataformas, Platos, Pletinas, Poleas, Pomos, Portacepillos, Portaestantes, Portallaves, Portarollos, Portavasos, Prensas, Producto para madera, Puntas, Punteros, Punzones, ba&amp;ntilde;os, cerrajer&amp;iacute;a, decoracion, decoraci&amp;oacute;n, ferreteria, herrajes, herramienta, interiorismo, manilla, metalisteria, pomos, productos ferreteria, zitro, zitroherrajes"></META>
-                <META id="_TopLeft_metaLanguage" NAME="Language" content="español - spanish"></META>
-                <META id="_TopLeft_metaRobots" NAME="Robots" content="All"></META>
+                <html>
+                    <head>
+                        <TITLE>Ferreteria Ortiz - Ferretería Ortiz</TITLE>
+                        <META id="_TopLeft_metaTitle" NAME="Title" content="Ferreteria Ortiz. Tienda online con más de 20.000 productos - Ferretería Ortiz"></META>
+                        <META id="_TopLeft_metaSubject" NAME="Subject" content="Ferretería Ortiz. Tienda online con más de 20.000 productos."></META>
+                        <META id="_TopLeft_metaDescription" NAME="Description" content="Ferretería Ortiz. Tienda online con más de 20.000 productos. Herrajes para obra y decoración, metalistería, cerrajería, accesorios para baño, herramientas, ferretería en general."></META>
+                        <META id="_TopLeft_metaKeywords" NAME="Keywords" content="Abrazaderas, Accesorios ba&amp;ntilde;o, Aceites, Adhesivos, Adornos, Alambres, Alcayatas, Alcotanas, Aldaba, Aldabilla, Alicates, Alzapa&amp;ntilde;os, Amaestramiento, Amoladoras, Angulo, Anillas, Antipanicos, Antirrobos, Aplacadoras, Apliques, Apliques para luz, Aprieto, Arandelas, Archivadores, Arcos de segueta, Arcos de sierra, Armeros, Asas, Aspiradores, Avellanadores, Barandilla, Barras, Barrenas, Baterias, Berbiquis, Bisagras, Bloqueador, Bocallaves, Bombillos, botiquines, Brocas, Burletes, Buzones, Cadenas, Cajas fuertes, Calibres, Campanas, Candados, Cantoneras, capitel, Cariatides, Carretilla, Carriles, Cartelas, casquillos, Cazoletas, Cepillos, Cerraderos, Cerraduras, Cerraduras mueble, Cerrojillos, Cerrojo, Cestas, Chapas, Cierrapuertas, Cierre, Cinceles, Cinturoneros, Cizallas, Clavadoras, Clavos, Colgantes, Collarines, Compases, Compresores, Condena, Corbateros, Coronas, Correderas, Cortador, Cortafrios, Cortatubos, Cremonas, Cuadradillo, Cuchillos, Cuerda, Cutters, Decaparadores, Desbloqueo, Destornilladores, Dosificadores, Dremel, Electricidad, Electrodos, Engletadoras, Ensambles, Escaleras, Escobilleros, Escofinas, Escuadras, Eses, Espatulas, Espejos, Fallebas, Fieltros, Figuras, Filete, Flesadoras, Flexometros, Formones, Ganchos, Garras, Garrotas, Gramiles, Granetes, Grapadoras, Grapas, Griferias, Grisans, Guantes, Gubias, Guias, Guillamenes, Hachas, Hembrillas, Herramientas, Husillos, Imanes, Ingletadoras, Jaboneras, Lavabos, Letras, Letreros, Lija, Lijadoras, Limas, Limpieza, Linternas, Llamadores, Llanas, Llaveros, Llaves, Lubricantes, Macetas, Macollas, Mamparas, Mangos, Manilla, Manillon, Martillos, Mascaras, Mazas, Mecanismos, Metopas, Mirillas, Moldura, Muebles, Muelas, Muelles, Muletillas, Multiusos, niveles, Nudos, Numeros, Paletas, Pantaloneros, Papeleras, Pasacables, Pasadores, Pasamanos, Pasamonedas, Patas, Pegamentos, Perchas, Pernios, Picaporte, Piedras, Pinchos, Pinzas, pistola, Placa limpieza, Plataformas, Platos, Pletinas, Poleas, Pomos, Portacepillos, Portaestantes, Portallaves, Portarollos, Portavasos, Prensas, Producto para madera, Puntas, Punteros, Punzones, ba&amp;ntilde;os, cerrajer&amp;iacute;a, decoracion, decoraci&amp;oacute;n, ferreteria, herrajes, herramienta, interiorismo, manilla, metalisteria, pomos, productos ferreteria, zitro, zitroherrajes"></META>
+                        <META id="_TopLeft_metaLanguage" NAME="Language" content="español - spanish"></META>
+                        <META id="_TopLeft_metaRobots" NAME="Robots" content="All"></META>
+                        <link href="css/ie.css" rel="stylesheet" type="text/css">
+                    </head>
 
-                <link href="css/ie.css" rel="stylesheet" type="text/css" >
-            </head>
+                    <body text=#000000 vLink=#0069b3 aLink=#ff0000 link=#0069b3 bgColor=#ffffff topMargin=10>
 
-            <BODY text=#000000 vLink=#0069b3 aLink=#ff0000 link=#0069b3 bgColor=#ffffff topMargin=10>
-
-                <table width="908" border="0" align="center" cellpadding="0" cellspacing="0">
+                        <table width="908" border="0" align="center" cellpadding="0" cellspacing="0">
                     <tr>
                         <td width="12" height="67"><img src="images/izq_sup_degradado.gif" width="12" height="67"></td>
                         <td width="156"><a href="index.php"><img src="images/logo.gif" width="156" height="67" border="0"></A></td>
@@ -42,15 +41,19 @@
                     <tr bgcolor="#F0F0F0">
                         <td>&nbsp;	</td>
                         <td><img src="images/logo_2.gif" width="156" height="26"></td>
-                        <td><table width="98%" border="0" cellspacing="0" cellpadding="2">
+                        <td>
+                            <table width="98%" border="0" cellspacing="0" cellpadding="2">
                                 <tr>
-                                    <td width="12%"><table width="55" border="0" align="center" cellpadding="0" cellspacing="0">
+                                    <td width="12%">
+                                        <table width="55" border="0" align="center" cellpadding="0" cellspacing="0">
                                             <tr>
                                                 <td><a href="http://www.facebook.com/pages/Ferreteria-Ortiz/330288487021071?ref=ts" target="_blank"><img src="images/facebook.gif" alt="Siguenos en facebook" width="23" height="23" border="0"></a></td>
                                                 <td><a href="http://www.elblogdeferreteriaortiz.es" target="_blank"><img src="images/blog.gif" alt="Siguenos en nuestro blog" width="23" height="23" border="0"></a></td>
                                             </tr>
-                                        </table></td>
-                                    <td width="64%"><table width="90%" border="0" align="right" cellpadding="0" cellspacing="0">
+                                        </table>
+                                    </td>
+                                    <td width="64%">
+                                        <table width="90%" border="0" align="right" cellpadding="0" cellspacing="0">
                                             <tr>
                                                 <td width="100%" valign="top">
                                                     <p align="LEFT">
@@ -62,7 +65,8 @@
                                                     </form>
                                                     </p></td>
                                             </tr>
-                                        </table></td>
+                                        </table>
+                                    </td>
                                     <td width="24%">
                                         <table width="100%" border="0" cellspacing="0" cellpadding="0">
                                             <tr>
@@ -70,85 +74,88 @@
                                                 <td width="26%" align="right" valign="middle">buscar:&nbsp;</td>
                                                 <td width="59%" align="right"><input NAME="find" TYPE="text" SIZE="20,1" MAXLENGTH="50" value=""></td>
                                                 <td width="15%" align="right"><input name="boton" type="image" src="images/head/ir.gif" width="20" height="18" border=0 value="Buscar"></td>
-                                            </FORM>
+                                            </form>
                                 </tr>
-                            </table></td>
+                                        </table>
+                                    </td>
 
-                    </tr>
-                </table></td>
-            <td>&nbsp;</td>
-            </tr>
-    </table>
-
-    <!--Base structure-->
-    <div align="center">
-        <table border="0" width="100%" cellspacing="2" cellpadding="0">
-            <tr>
-                <td bgcolor="#ffffff" width="168" align="left" valign="top">
-                    <!--/Base structure-->
-                    <!--FRANJA IZQUIERDA-->
-                    <?php
-                    require 'franjaIzquierda.php';
-                    ?>
-                    <!--FIN FRANJA IZQUIERDA-->
-                    <br>
-                    <br>
-                    <br>
-                </td>
-                <td width="100%" align="left" valign="top">&nbsp;
-                    <P>
-
-                        <map name="Map">
-                            <area shape="rect" coords="127,8,171,25" href="category.aspx?special=O" alt="Ofertas">
-                            <area shape="rect" coords="183,9,256,25" href="category.aspx?special=N" alt="Novedades">
-                            <area shape="rect" coords="265,8,360,25" href="category.aspx?special=MS" alt="Los m&aacute;s vendidos">
-                            <area shape="rect" coords="367,9,434,25" href="a2z.aspx" alt="Listado AZ">
-                        </map>
-
-                        <SCRIPT LANGUAGE="JavaScript">
-                            function popUp(url) {
-                                sealWin = window.open(url, "win", 'toolbar=0,location=0,directories=0,status=1,menubar=1,scrollbars=1,resizable=1,width=500,height=450');
-                                self.name = "mainWin";
-                            }
-                        </SCRIPT>
-
-                        <!--/Content area-->
+                                </tr>
+                            </table>
+                        </td>
+                        <td>&nbsp;</td>
+                     </tr>
+                </table>
 
                         <!--Base structure-->
-                </td>
+                        <div align="center">
+                            <table border="0" width="100%" cellspacing="2" cellpadding="0">
+                                <tr>
+                                    <td bgcolor="#ffffff" width="168" align="left" valign="top">
+                                        <!--/Base structure-->
+                                        <!--FRANJA IZQUIERDA-->
+                                        <?php
+                                        require 'franjaIzquierda.php';
+                                        ?>
+                                        <!--FIN FRANJA IZQUIERDA-->
+                                        <br>
+                                        <br>
+                                        <br>
+                                    </td>
+                                    <td width="100%" align="left" valign="top">&nbsp;
+                                        <P>
 
-                <!--/Base structure-->
+                                            <map name="Map">
+                                                <area shape="rect" coords="127,8,171,25" href="category.aspx?special=O" alt="Ofertas">
+                                                <area shape="rect" coords="183,9,256,25" href="category.aspx?special=N" alt="Novedades">
+                                                <area shape="rect" coords="265,8,360,25" href="category.aspx?special=MS" alt="Los m&aacute;s vendidos">
+                                                <area shape="rect" coords="367,9,434,25" href="a2z.aspx" alt="Listado AZ">
+                                            </map>
 
-                <!--RIGHT-->
-                <td bgcolor="#ffffff" width="168" align="left" valign="top">
-                    <!--/Base structure-->
-                    <!--FRANJA DERECHA-->
-                    <?php
-                    require './franjaDerecha.php';
-                    ?>
-                    <!--FIN FRANJA DERECHA-->
-                    <br>
-                    <br>
-                    <br>
-                </td>
+                                            <SCRIPT LANGUAGE="JavaScript">
+                                                function popUp(url) {
+                                                    sealWin = window.open(url, "win", 'toolbar=0,location=0,directories=0,status=1,menubar=1,scrollbars=1,resizable=1,width=500,height=450');
+                                                    self.name = "mainWin";
+                                                }
+                                            </SCRIPT>
 
-            </tr>
-        </table>
-    </div>
-    <!--/Base structure-->
+                                            <!--/Content area-->
 
-    <!--BASE-->
-    <?php
-    require 'franjaBase.php';
-    ?>
-    <!--BASE-->
+                                            <!--Base structure-->
+                                    </td>
 
-</BODY>
-</HTML>	</td>
-</tr>
-</table>
+                                    <!--/Base structure-->
+
+                                    <!--RIGHT-->
+                                    <td bgcolor="#ffffff" width="168" align="left" valign="top">
+                                        <!--/Base structure-->
+                                        <!--FRANJA DERECHA-->
+                                        <?php
+                                        require './franjaDerecha.php';
+                                        ?>
+                                        <!--FIN FRANJA DERECHA-->
+                                        <br>
+                                        <br>
+                                        <br>
+                                    </td>
+
+                                </tr>
+                            </table>
+                        </div>
+                        <!--/Base structure-->
+
+                        <!--BASE-->
+                        <?php
+                        require 'franjaBase.php';
+                        ?>
+                        <!--BASE-->
+
+                    </body>
+                </html>	
+            </td>
+        </tr>
+    </table>
 <map name="Map">
     <area shape="circle" coords="434,35,49" href="http://www.ferreteriaortiz.es/store/promo_nespresso.pdf" target="_blank">
     <area shape="rect" coords="4,2,373,79" href="http://www.ferreteriaortiz.es/store/item.aspx?item_id=0309.0087">
 </map>
-</BODY>
+</body>
