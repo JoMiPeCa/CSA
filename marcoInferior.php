2778ecf02360d34afb30b4692eda4f3189d64159
Joseph <jose.perez@peluka.info>
2016-06-23 20:21:10 -0300

    Links actualizados

--- marcoInferior.php
+++ marcoInferior.php
@@ -13,7 +13,7 @@
                         <area shape="rect" coords="127,8,171,25" href="#" alt="Ofertas">
                         <area shape="rect" coords="183,9,256,25" href="#" alt="Novedades">
                         <area shape="rect" coords="265,8,360,25" href="#" alt="Los m&aacute;s vendidos">
-                        <area shape="rect" coords="367,9,434,25" href="#" alt="Listado AZ">
+                        <area shape="rect" coords="367,9,434,25" href="catalogo.php" alt="Listado AZ">
                     </map>
                 </div>
                 <?php require 'franjaBase.php'; ?>
--- marcoSuperior.php
+++ marcoSuperior.php
@@ -21,7 +21,7 @@
                                 <tr>
                                     <td width="66%" align="center"><img src="images/lomasdelcatalogo.gif" width="445" height="31" border="0" usemap="#Map"></td>
                                     <td width="17%" align="center"><a href="login.php"><img src="images/areadecliente.gif" alt="&Aacute;rea de cliente" width="124" height="31" border="0"></a></td>
-                                    <td width="17%" align="center"><a href="#"><img src="images/micesta.gif" alt="Mi cesta" width="110" height="31" border="0"></a></td>
+                                    <td width="17%" align="center"><a href="carroCompra.php"><img src="images/micesta.gif" alt="Mi cesta" width="110" height="31" border="0"></a></td>
                                 </tr>
                             </table>
                             <!-- FIN CABECERA SECCION CENTRAL-->
