c5572217c24581d6caa9aaa74e4dbd5ea44eba8c
Jonathan Espinoza <jonathanespinozab@gmail.com>
2016-06-23 23:12:19 -0300

    J.E.B. Registro de usuario dividido en capas

---
+++ form/form_registro.php
@@ -0,0 +1,79 @@
+<?php
+$usr = new Cliente();
+?>
+
+<center>
+    <div id="titlogin"><legend>Registro de usuario.</legend>            
+        <table>
+            <form action="accForm/accRegistro.php" method="post">
+                <!-- private $srun;
+                private $snombre;
+                private $apellidos;
+                private $csexo;
+                private $dfechanacimiento;
+                private $semail;
+                private $bSuscripcion;
+                private $nTelefono;
+                private $spass;
+                -->
+                <tr>
+                    <td>Nombre:</td>
+                    <td><input name="nombre" id="nombre" type=text required="true"></td>
+                    <td> </td>
+                </tr>
+                <tr>                       
+                    <td>Apellidos:</td>
+                    <td><input name="apellidos" id="apellidos" type="text" required="true"></td>
+                    <td> </td>
+                </tr>
+                <tr>                       
+                    <td>RUN:</td>
+                    <td><input name="run" id="run" type="text" required="true"></td>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <td>Género:</td>
+                    <td><select id="genero" name="genero" required="true">
+                            <option value="">Seleccione...</option>
+                            <option value="1">Mujer</option>
+                            <option value="2">Hombre</option>
+                        </select></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Fecha Nacimiento:</td>
+                    <td><input type="date" id="fecha" name="fecha" required="true"></td>
+                    <td> </td>
+                </tr>  
+                <tr>
+                    <td>Mail:</td>
+                    <td><input type="email" id="correo" name="correo" required="true"></td>
+                    <td> </td>
+                </tr>                    
+                <tr>
+                    <td>Teléfono:</td>
+                    <td><input type="number" id="telefono" name="telefono" required="true"></td>
+                    <td> </td>
+                </tr>  
+                <tr>
+                    <td>Contraseña:</td>
+                    <td><input type="password" id="pass" name="pass" required="true"></td>
+                    <td> </td>
+                    <td>Repita Contraseña:</td>
+                    <td><input type="password" id="pass" name="pass" required="true"></td>
+                    <td> </td>
+                </tr>  
+                <tr>
+                    <td>Desea suscribirse a nuestro boletín mensual?:</td>
+                    <td>Si<input type="radio" id="suscripcion" name="suscripcion" checked="true" >
+                        No<input type="radio" id="suscripcion" name="suscripcion"></td>
+                    <td></td>
+                </tr>  
+                <tr>
+                    <td></td>  
+                    <td><input id="registrar" type="submit" value="Registrar"></td>
+                </tr>
+            </form>
+        </table>
+    </div>
+</center>
--- librerias.php
+++ librerias.php
@@ -1,7 +1,7 @@
 <?php
 include ('constantes.php');
 include (PATHAPP.'lib/db_funciones.php');
-include (PATHAPP.'clases/Cliente.php');
+include (PATHAPP.'./clases/Cliente.php');
 include (PATHAPP.'clases/Administrador.php');
 include (PATHAPP.'clases/Producto.php');
 ?>
