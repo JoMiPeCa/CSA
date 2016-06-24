09874a78ede98e456c3ba2e80af747a477d203d1
Jonathan Espinoza <jonathanespinozab@gmail.com>
2016-06-23 22:44:39 -0300

    J.E.B. Avance registro de usuario

--- accForm/accRegistro.php
+++ accForm/accRegistro.php
@@ -1,11 +1,11 @@
 <?php
-include 'librerias.php';
-$usr = new Cliente($_POST["run"], $_POST["nombre"], $_POST["apellidos"], $_POST["fecha"], $_POST["correo"], $_POST["suscripcion"], $_POST["telefono"], $_POST["contraseña"]);
+include ('../librerias.php');
+$usr = new Cliente($_POST["run"], $_POST["nombre"], $_POST["apellidos"], $_POST["fecha"], $_POST["correo"], $_POST["suscripcion"], $_POST["telefono"], $_POST["pass"]);
 session_start();
-if ($usr->VerificaAcceso()) {
+if ($usr->CreaCliente()) {
     $_SESSION["oUsuario"] = $usr;
 }
 ?>
 <script>
-    document.location.href = "index.php";
+    document.location.href = ('carroCompra.php');
 </script>
