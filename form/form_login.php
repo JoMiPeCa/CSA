f393c95e0f1f2dc2e871e20a8ddd623ae9b7ebaa
Jonathan Espinoza <jonathanespinozab@gmail.com>
2016-06-24 22:53:34 -0300

    J.E.B. Edicion registro y logueo de usuario.

---
+++ form/form_login.php
@@ -0,0 +1,16 @@
+<center>
+    <div id="titlogin">
+        Acceso de usuario.
+        <form action="../accForm/accLogin.php" method="POST" id="loginForm">
+            <div>Usuario:</div>
+            <div><input name="usuario" type=text required="true"></div></br>
+            <div>Clave:</div>
+            <div><input name="clave" type="password" required="true"></div></br>
+            <input id="acceder" type="submit" value="Acceder"></br>   
+        </form>
+
+        <form action="registroUsuario.php">
+            <input id="registro" type="submit" value="Registrar">
+        </form>
+    </div>
+</center>
