ea9ac8083bdd4e8da20f458e02164c716d464038
Jorge Munoz <CorreodelJorge@Correo.Com>
2016-06-30 14:47:20 -0300

    Jorge Muñoz Mensaje excepción en login.

--- accForm/accLogin.php
+++ accForm/accLogin.php
@@ -6,9 +6,24 @@ $usr = new Cliente("", "", "", "", "", $_POST['usuario'], "", "", $clavemd5);
 
 session_start();
 if ($usr->VerificaAcceso()) {
-$_SESSION["oUsuario"] = $usr;
+    $_SESSION["oUsuario"] = $usr;
+    ?>
+    <script>
+        document.location.href = "<?= PATHURL ?>index.php";
+    </script>
+    <?php
+} else {
+    ?>
+    <script type="text/javascript">
+        alert("Usuario o contraseña incorrectos. Favor intente nuevamente...");
+    </script>
+    
+    <script>
+        document.location.href = "<?= PATHURL ?>login.php";
+    </script>
+
+    
+    <?php
 }
 ?>
-<script>
-document.location.href = "<?= PATHURL ?>index.php";
-</script>
+
